b100aa661a55052dd3d9fa1753ef951304b6c6b2
Julian Veit <claymore1298@gmail.com>
2020-03-11 11:42:48 +0100

    sdm845-common: remove touch hal

--- common.mk
+++ common.mk
@@ -165,11 +165,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Touch
-PRODUCT_PACKAGES += \
-    lineage.touch@1.0-service.oneplus_sdm845 \
-    TouchGestures
-
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
